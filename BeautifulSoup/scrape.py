fc5e8dd83eeaffaae110807b29ab791a0ec699c7
Corey Schafer <CoreyMSchafer@gmail.com>
2017-11-07 22:14:25 -0700

    BeautifulSoup Tutorial

---
+++ BeautifulSoup/scrape.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+source = requests.get('http://coreyms.com').text
+
+soup = BeautifulSoup(source, 'lxml')
+
+csv_file = open('cms_scrape.csv', 'w')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline', 'summary', 'video_link'])
+
+for article in soup.find_all('article'):
+    headline = article.h2.a.text
+    print(headline)
+
+    summary = article.find('div', class_='entry-content').p.text
+    print(summary)
+
+    try:
+        vid_src = article.find('iframe', class_='youtube-player')['src']
+
+        vid_id = vid_src.split('/')[4]
+        vid_id = vid_id.split('?')[0]
+
+        yt_link = f'https://youtube.com/watch?v={vid_id}'
+    except Exception as e:
+        yt_link = None
+
+    print(yt_link)
+
+    print()
+
+    csv_writer.writerow([headline, summary, yt_link])
+
+csv_file.close()
