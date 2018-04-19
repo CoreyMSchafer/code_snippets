59afafb9f627f67d1863b82eeb838d37915924c7
Seamus <seamusdemora@gmail.com>
2018-04-19 04:09:14 +0100

    Resolve error re data type
    For reasons I don't understand, the call returns something other than a string data type. This converts the data to string.

--- Python-JSON/api.py
+++ Python-JSON/api.py
@@ -2,7 +2,7 @@ import json
 from urllib.request import urlopen
 
 with urlopen("https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json") as response:
-    source = response.read()
+    source = response.read().decode('utf-8')
 
 data = json.loads(source)
 
