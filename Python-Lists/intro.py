50e9d2cb7c543949d38ad7ac6ec5a938775c7765
Yann Mulonda <yannmjl@gmail.com>
2018-05-01 17:14:52 -0500

    added list operations

--- Python-Lists/intro.py
+++ Python-Lists/intro.py
@@ -1,6 +1,18 @@
 # Python 3.6 Tutorial for Beginners
 # Working with Lists, Tuples, and Sets
- 
+
+#---------------------------------------------------------------------------------------------#
+# basic arithmetic operation                                                                  #
+#---------------------------------------------------------------------------------------------#
+# let's create a list of fruits
+fruits = ['bananas', 'mango', 'blueberry', 'apple', 'sherry', 'strawberry']
+print(fruits)
+
+#get the number of item in the list
+print('The number of items in the list is:', len(fruits))
+
+#print an item in the list using index
+print('the first item in the list is:', fruits[0])
 # Empty Lists
 empty_list = []
 empty_list = list()
