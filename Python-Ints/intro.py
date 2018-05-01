6094cad3aa4ae7f0be04bd73eec769c29d0bcaa2
Yann Mulonda <yannmjl@gmail.com>
2018-05-01 16:23:23 -0500

    added basic arthmetic operation

--- Python-Ints/intro.py
+++ Python-Ints/intro.py
@@ -12,3 +12,35 @@ num4 = -6
 # number that get set value of number within a string
 num5 = '86'
 num6 = '28'
+
+#---------------------------------------------------------------------------------------------#
+# basic arithmetic operation                                                                  #
+#---------------------------------------------------------------------------------------------#
+# addition
+add = num1 + num2
+print(f'{num1} + {num2} =', add)
+
+# subtraction
+sub = num1 - num2
+print(f'{num1} - {num2} =', sub)
+
+# multiplication
+multi = num1 * num2
+print(f'{num1} * {num2} =', multi)
+
+# division in python 3 give us a decimal(float) 
+# eventhough we divided two intergers 
+div = num1 / num2
+print(f'{num1} / {num2}=', div)
+
+# floor division drops the dicimal and only return an integer
+floor_div = num1 // num2
+print(f'{num1} // {num2} =', floor_div)
+
+# exponent
+expo = num1 ** num2
+print(f'{num1} ** {num2} =', expo)
+
+# modulus
+modu = num1 % num2
+print(f'{num1} % {num2} =', modu)
