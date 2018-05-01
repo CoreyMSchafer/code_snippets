c68dbfc22058ea242e91db0513d0a86be1b9e45a
Yann Mulonda <yannmjl@gmail.com>
2018-05-01 16:17:27 -0500

    set integer variable for the tutorial

--- Python-Ints/intro.py
+++ Python-Ints/intro.py
@@ -1,8 +1,14 @@
 
-num_1 = '100'
-num_2 = '200'
+# Python 3.6 Tutorial for Beginners
+# Working with Numeric Data
+# Integers and Floats 
 
-num_1 = int(num_1)
-num_2 = int(num_2)
-
-print(num_1 + num_2)
+#let's set some integer numbers
+num = 4
+num1 = 5
+num2 = 3
+num3 = 3.68
+num4 = -6
+# number that get set value of number within a string
+num5 = '86'
+num6 = '28'
