3e23f3ecc7f6ff0a8a1b55df297673d0618e9137
Corey Schafer <CoreyMSchafer@gmail.com>
2018-06-20 10:22:07 -0600

    Python Sets Snippets

---
+++ Python/Python-Sets/sets.py
@@ -0,0 +1,12 @@
+
+employees = ['Corey', 'Jim', 'Steven', 'April', 'Judy', 'Jenn', 'John', 'Jane']
+
+gym_members = ['April', 'John', 'Corey']
+
+developers = ['Judy', 'Corey', 'Steven', 'Jane', 'April']
+
+if 'Corey' in developers:
+    print('Found!')
+
+# O(n) for list
+# O(1) for a set
