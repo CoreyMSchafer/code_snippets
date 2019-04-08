662f1d80ba0321929556e06698f348a9df6790b6
fizzer666 <m.farrington1980@gmail.com>
2019-04-08 21:08:15 +0100

    Update config.py
    updates to config.py so that its backwards compatible with the previous lessons.  Will load the config.json file first and fall back to environ variables if the file does not exist.

--- Python/Flask_Blog/13-Deployment-Linode/flaskblog/config.py
+++ Python/Flask_Blog/13-Deployment-Linode/flaskblog/config.py
@@ -1,11 +1,18 @@
+import json
 import os
+
+try:
+    with open('/etc/config.json') as config_file:
+        config = json.load(config_file)
+except FileNotFoundError:
+    config = os.environ
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = config.get('EMAIL_USER')
+    MAIL_PASSWORD = config.get('EMAIL_PASS')
