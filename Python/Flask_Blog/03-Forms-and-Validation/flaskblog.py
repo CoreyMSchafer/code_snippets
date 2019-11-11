fb74134f99391f03cccf63f689ec1d81599bc188
Atul Malakar <atulmalakar15@gmail.com>
2019-11-11 19:24:03 +0530

    Update at line number 38.
    Minor typo that was problematic !

--- Python/Flask_Blog/03-Forms-and-Validation/flaskblog.py
+++ Python/Flask_Blog/03-Forms-and-Validation/flaskblog.py
@@ -35,7 +35,7 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
+        flash('Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
