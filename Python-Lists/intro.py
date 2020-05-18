4f4f34dc4d19273b966842c18a8f4c806b29a805
ljungster <parkerljung@gmail.com>
2020-05-18 15:32:44 -0500

    testing push

--- Python-Lists/intro.py
+++ Python-Lists/intro.py
@@ -6,7 +6,8 @@ empty_list = list()
 # Empty Tuples
 empty_tuple = ()
 empty_tuple = tuple()
+new_change = 10
 
 # Empty Sets
-empty_set = {} # This isn't right! It's a dict
+empty_set = {}  # This isn't right! It's a dict
 empty_set = set()
