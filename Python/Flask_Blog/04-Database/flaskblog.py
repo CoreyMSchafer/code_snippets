615ca4dd48e0c972dfbc5d294a3a5b35bfbaa025
Sayan3sarkar <sayansarkar333@gmail.com>
2020-07-20 23:21:56 +0530

    Added alternate code

--- Python/Flask_Blog/04-Database/flaskblog.py
+++ Python/Flask_Blog/04-Database/flaskblog.py
@@ -6,6 +6,9 @@ from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# Alternate path for SQLALCHEMY_DATABASE_URI(works for Windows)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.path.join(os.path.dirname(__file__), 'site.db')
 db = SQLAlchemy(app)
 
 
