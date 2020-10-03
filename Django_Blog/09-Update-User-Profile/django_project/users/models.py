03ae74f3448a14a678f60472a2545d7b37642f6b
Shubham Aggarwal <shubham2403tayal@gmail.com>
2020-10-03 18:17:30 +0530

    Update models.py

--- Django_Blog/09-Update-User-Profile/django_project/users/models.py
+++ Django_Blog/09-Update-User-Profile/django_project/users/models.py
@@ -10,12 +10,12 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
+            output_size = (300,300)
             img.thumbnail(output_size)
             img.save(self.image.path)
