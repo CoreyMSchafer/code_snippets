fe6a3d1a36db2cb6afdfe5578f2c736f49ec377d
anunda-nyagami <davis.anunda@gmail.com>
2022-05-18 18:57:29 +0300

    Updated README.md with links. Edited string interpolation folder name by adding a dash between the two words

--- String Interpolation/test.py
+++
@@ -1,9 +0,0 @@
-name = 'Corey'
-age = 28
-
-# greeting = 'My name is ' + name + ' and I am ' + str(age) + ' years old'
-
-greeting = 'I am {age} years old and my name is {name}'.format(name=name, age=age)
-
-print greeting
-
