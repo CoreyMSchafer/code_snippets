e9b46a8c64c71eaa6b8bbfe38da9118018cd260d
MedAmineJH <medaminejhaidri@gmail.com>
2023-01-07 12:44:15 +0100

    add if __name__ == '__main__

--- Python/MultiProcessing/multiprocessing-demo.py
+++ Python/MultiProcessing/multiprocessing-demo.py
@@ -1,7 +1,7 @@
 import concurrent.futures
 import time
 
-start = time.perf_counter()
+
 
 
 def do_something(seconds):
@@ -10,13 +10,19 @@ def do_something(seconds):
     return f'Done Sleeping...{seconds}'
 
 
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    secs = [5, 4, 3, 2, 1]
-    results = executor.map(do_something, secs)
 
-    # for result in results:
-    #     print(result)
+if __name__ == '__main__':
 
-finish = time.perf_counter()
+    start = time.perf_counter()
 
-print(f'Finished in {round(finish-start, 2)} second(s)')
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        secs = [5, 4, 3, 2, 1]
+        results = executor.map(do_something, secs)
+
+    for result in results:
+        print(result)
+
+    finish = time.perf_counter()
+
+    print(f'Finished in {round(finish-start, 2)} second(s)')
