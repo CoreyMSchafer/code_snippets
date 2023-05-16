f2f3540d5127a74c722d142c2cf9e612acd6d718
nimstepf <nicolasimstepf@gmail.com>
2023-05-16 16:03:00 +0200

    apply app_context

--- Python/Flask_Blog/05-Package-Structure/flaskblog/__init__.py
+++ Python/Flask_Blog/05-Package-Structure/flaskblog/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+with app.app_context():
+    db = SQLAlchemy(app)
 
 from flaskblog import routes
