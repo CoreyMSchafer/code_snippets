e75811612cf0680c0b19c4d02e91423befbd316a
rohit singh <rohit2701singh@gmail.com>
2024-03-04 13:48:10 +0530

    layout bootstrap, logout user, user models file update

--- Django_Blog/12-Password-Reset/django_project/users/models.py
+++ Django_Blog/12-Password-Reset/django_project/users/models.py
@@ -10,8 +10,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
